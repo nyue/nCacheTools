955267b812de4e90b5ee49880ab5cfe4feb5ac3f
Nicholas Yue <yue.nicholas@gmail.com>
2016-05-04 22:06:28 -0700

    Export Houdini CSV format file, verified with Mantra velocity motion blur

--- apps/mcc2csv_main.cpp
+++ apps/mcc2csv_main.cpp
@@ -21,65 +21,69 @@ int main(int argc, char** argv)
 	const nCache::ChannelDataContainer& channels_data = mc_reader.get_channels_data();
 	if (!channels_data.empty())
 	{
-	nCache::ChannelDataContainer::const_iterator iter = channels_data.begin();
-	nCache::ChannelDataContainer::const_iterator eIter = channels_data.end();
-	int64_t num_rows = -1;
-	bool num_rows_obtained = false;
-	// Print CSV header
-	for (;iter!=eIter;++iter)
-	{
-		// std::cout << boost::format("Channel['%1%'] type = %2%") % iter->first % nCache::ChannelDataType2string(iter->second._type) << std::endl;
-		switch (iter->second._type)
+		nCache::ChannelDataContainer::const_iterator iter;
+		nCache::ChannelDataContainer::const_iterator eIter = channels_data.end();
+		int64_t num_rows = -1;
+		bool num_rows_obtained = false;
+		// Print CSV header
+		for (iter = channels_data.begin();iter!=eIter;++iter)
 		{
-		case nCache::DBLA :
-			csv_file << boost::format("%1%,") % iter->first;
-			if (!num_rows_obtained)
+			// std::cout << boost::format("Channel['%1%'] type = %2%") % iter->first % nCache::ChannelDataType2string(iter->second._type) << std::endl;
+			switch (iter->second._type)
 			{
-				num_rows = iter->second._dbla.size();
-				num_rows_obtained = true;
+			case nCache::DBLA :
+				csv_file << boost::format("%1%,") % iter->first;
+				if (!num_rows_obtained)
+				{
+					num_rows = iter->second._dbla.size();
+					num_rows_obtained = true;
+				}
+				break;
+			case nCache::FVCA :
+				csv_file << boost::format("%1%.x,%1%.y,%1%.z,") % iter->first;
+				if (!num_rows_obtained)
+				{
+					num_rows = iter->second._fvca.size();
+					num_rows_obtained = true;
+				}
+				break;
+			case nCache::FBCA :
+				csv_file << boost::format("%1%,") % iter->first;
+				if (!num_rows_obtained)
+				{
+					num_rows = iter->second._fbca.size();
+					num_rows_obtained = true;
+				}
+				break;
 			}
-			break;
-		case nCache::FVCA :
-			csv_file << boost::format("%1%.x,%1%.y,%1%.z,") % iter->first;
-			if (!num_rows_obtained)
-			{
-				num_rows = iter->second._fvca.size();
-				num_rows_obtained = true;
-			}
-			break;
-		case nCache::FBCA :
-			csv_file << boost::format("%1%,") % iter->first;
-			if (!num_rows_obtained)
-			{
-				num_rows = iter->second._fbca.size();
-				num_rows_obtained = true;
-			}
-			break;
 		}
-	}
-	if (num_rows < 0)
-		throw std::runtime_error("Negative rows");
+		csv_file << std::endl;
+		if (num_rows < 0)
+			throw std::runtime_error("Negative rows");
 
-	// Print output data
-	for (;iter!=eIter;++iter)
-	{
-		switch (iter->second._type)
+		// Print output data
+		for (int64_t row_index=0;row_index<num_rows;row_index++)
 		{
-		case nCache::DBLA :
-			csv_file << boost::format("%1%,") % iter->first;
-			break;
-		case nCache::FVCA :
-			csv_file << boost::format("%1%.x,%1%.y,%1%.z,") % iter->first;
-			break;
-		case nCache::FBCA :
-			csv_file << boost::format("%1%,") % iter->first;
-			break;
+			// Iterate the column
+			for (iter = channels_data.begin();iter!=eIter;++iter)
+			{
+				switch (iter->second._type)
+				{
+				case nCache::DBLA :
+					csv_file << boost::format("%1%,") % iter->second._dbla[row_index];
+					break;
+				case nCache::FVCA :
+					csv_file << boost::format("%1%,%2%,%3%,") % iter->second._fvca[row_index].x % iter->second._fvca[row_index].y % iter->second._fvca[row_index].z ;
+					break;
+				case nCache::FBCA :
+					csv_file << boost::format("%1%,") % iter->second._fbca[row_index];
+					break;
+				}
+
+			}
+			csv_file << std::endl;
 		}
-
-	}
-
-	csv_file << std::endl;
-	csv_file.close();
+		csv_file.close();
 	}
 	return 0;
 }
