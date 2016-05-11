3ffab0a3f8431f41ffa9c041288764e4e23e2490
Nicholas Yue <yue.nicholas@gmail.com>
2016-05-11 19:52:43 +1000

    Adding alembic exporter

--- apps/nCache2alembic_main.cpp
+++ apps/nCache2alembic_main.cpp
@@ -1,4 +1,3 @@
-// /corp/projects/surfup2/work/show/tst010/310/fx/hraab/maya/cache/nCache/fx_hraab_wave_v001
 #include <iostream>
 #include <string>
 #include <boost/format.hpp>
@@ -11,12 +10,16 @@ int
 main (int argc, char **argv)
 {
 	google::InitGoogleLogging(argv[0]);
-	if (argc == 2)
+	if (argc != 3)
 	{
-		std::string xml_filename(argv[1]);
-		nCache::nCacheLoader cache_loader(xml_filename);
-		cache_loader.process();
+		std::cerr << boost::format("Usage : %1% <input-ncache-xml> <output-alembic-file>") % argv[0] << std::endl;
+		return 1;
 	}
+	std::string xml_filename(argv[1]);
+	std::string alembic_filename(argv[2]);
+	nCache::nCacheLoader cache_loader(xml_filename);
+	cache_loader.process();
+
 	return 0;
 }
 
